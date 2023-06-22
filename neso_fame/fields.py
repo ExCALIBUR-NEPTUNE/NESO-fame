1f1c363aff4fb6893e1d9b77df8109dd4aa8434d
Chris MacMackin <chris.macmackin@ukaea.uk>
2023-06-22 18:03:05 +0100

    Improved distance calculations for fake magnetic fields

--- neso_fame/fields.py
+++ neso_fame/fields.py
@@ -3,7 +3,7 @@ import numpy.typing as npt
 
 from neso_fame.mesh import (
     FieldTrace,
-    C,
+    CoordinateSystem,
     CoordinateSystem,
     SliceCoord,
     SliceCoords,
@@ -13,44 +13,54 @@ from neso_fame.mesh import (
 def _cylindrical_distance(x1_start, angle, x3):
     a = x1_start
     b = np.tan(angle)
-    d = (a + b * x3)
+    d = a + b * x3
     c = np.sqrt(d**2 + b**2)
     c0 = np.sqrt(a**2 + b**2)
     if np.abs(angle) < 1e-8:
         return a * x3
     else:
-        return c * d / (2 * b) + 0.5 * b * np.log(c + d) - (c0 * a / (2*b) + 0.5 * b * np.log(c0 + a))
+        return (
+            c * d / (2 * b)
+            + 0.5 * b * np.log(c + d)
+            - (c0 * a / (2 * b) + 0.5 * b * np.log(c0 + a))
+        )
 
-def straight_field(angle=0.0) -> "FieldTrace[C]":
+
+def straight_field(angle=0.0) -> "FieldTrace":
     """Returns a field trace corresponding to straight field lines
     slanted at `angle` above the direction of extrusion into the first
     coordinate direction."""
 
     def trace(
-        start: SliceCoord[C], perpendicular_coord: npt.ArrayLike
-    ) -> tuple[SliceCoords[C], npt.NDArray]:
+        start: SliceCoord, perpendicular_coord: npt.ArrayLike
+    ) -> tuple[SliceCoords, npt.NDArray]:
         """Returns a trace for a straight field line."""
         x1 = start.x1 + perpendicular_coord * np.tan(angle)
         x2 = np.asarray(start.x2)
-        return SliceCoords(x1, x2, start.system), _cylindrical_distance(start.x1, angle, perpendicular_coord) if start.system == CoordinateSystem.Cylindrical else np.asarray(
+        return SliceCoords(x1, x2, start.system), _cylindrical_distance(
+            start.x1, angle, perpendicular_coord
+        ) if start.system == CoordinateSystem.Cylindrical else np.asarray(
             perpendicular_coord
-        ) / np.cos(angle)
+        ) / np.cos(
+            angle
+        )
 
     return trace
 
 
-def curved_field(weight=0.0) -> "FieldTrace[C]":
+def curved_field(weight=0.0) -> "FieldTrace":
     """Returns a field trace corresponding to straight field lines
     slanted at `angle` above the direction of extrusion into the first
     coordinate direction."""
 
     def trace(
-        start: SliceCoord[C], perpendicular_coord: npt.ArrayLike
-    ) -> tuple[SliceCoords[C], npt.NDArray]:
+        start: SliceCoord, perpendicular_coord: npt.ArrayLike
+    ) -> tuple[SliceCoords, npt.NDArray]:
         """Returns a trace for a straight field line."""
         x3 = np.asarray(perpendicular_coord)
-        x1 = start.x1 + weight * x3**2 - weight * x3
+        x1 = start.x1 + weight * x3**2  # - weight * x3
         x2 = np.asarray(start.x2)
+        # FIXME: The expression for distance here is wrong
         return SliceCoords(x1, x2, start.system), np.sign(x3) * (
             np.sqrt(4 * weight**2 * x3**2 + 1) ** 3 / (12 * weight**2)
             - 1 / (12 * weight**2)
