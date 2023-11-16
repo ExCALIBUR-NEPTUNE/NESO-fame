Made calculation of test equilibria lazy and fixed eqdsk reader test

neso_fame/hypnotoad_interface.py
@@ -676,12 +676,11 @@ def flux_surface_edge(
     if south.system != CoordinateSystem.CYLINDRICAL:
         raise ValueError("Coordinate system of `south` must be CYLINDRICAL")
 
-    # FIXME: I don't think this will be able to handle the inexact
-    # seperatrix you will get when doing a realistic connected double
-    # null
+    # FIXME: This won't be able to handle the inexact seperatrix you
+    # will get when doing a realistic connected double null
     psi_north = cast(float, eq.psi(north.x1, north.x2))
     psi_south = cast(float, eq.psi(south.x1, south.x2))
-    if not np.isclose(psi_north, psi_south, 1e-10, 1e-10):
+    if not np.isclose(psi_north, psi_south, 1e-6, 1e-6):
         raise ValueError(
             f"Start and end points {north} and {south} have different psi values "
             f"{psi_north} and {psi_south}"

tests/test_generators.py
@@ -10,7 +10,7 @@ from neso_fame.mesh import (
     control_points,
 )
 
-from .test_hypnotoad import CONNECTED_DOUBLE_NULL
+from .test_hypnotoad import CONNECTED_DOUBLE_NULL, to_mesh
 
 
 # Test for simple grid
@@ -790,9 +790,9 @@ def test_subdivided_grid_3d() -> None:
 
 
 def test_extruding_hypnotoad_mesh() -> None:
-    hypno_mesh = CONNECTED_DOUBLE_NULL
+    hypno_mesh = to_mesh(CONNECTED_DOUBLE_NULL)
     eq = hypno_mesh.equilibrium
-    mesh = generators.hypnotoad_mesh(hypno_mesh, (0.0, 0.125 * np.pi), 8, 21)
+    mesh = generators.hypnotoad_mesh(hypno_mesh, (0.0, 0.01 * np.pi), 8, 21)
     actual_nodes = frozenset(
         itertools.chain.from_iterable(
             (q.shape(0.0).to_coord(), q.shape(1.0).to_coord())