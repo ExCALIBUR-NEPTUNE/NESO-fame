ebb6b3cd5d0cba0c8d548f0b94fd54165db57fdc
Chris MacMackin <chris.macmackin@ukaea.uk>
2023-11-27 15:06:33 +0000

    Fixed formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -245,9 +245,7 @@ def trapezoidal_quad(
         starts[0][0] + (starts[0][0] - starts[1][0]) / 2,
         starts[0][1] + (starts[0][1] - starts[1][1]) / 2,
     )
-    if c == mesh.CoordinateSystem.CYLINDRICAL and (
-        starts[0][0] * starts[1][0] <= 0.0
-    ):
+    if c == mesh.CoordinateSystem.CYLINDRICAL and (starts[0][0] * starts[1][0] <= 0.0):
         return None
     shape = mesh.StraightLineAcrossField(
         mesh.SliceCoord(starts[0][0], starts[0][1], c),
