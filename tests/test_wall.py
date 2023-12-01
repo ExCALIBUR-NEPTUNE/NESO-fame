1e3e0c91a6f3b4a76890bd2aaa593070a30fbbe0
Chris MacMackin <chris.macmackin@ukaea.uk>
2023-12-01 17:56:05 +0000

    Added tests for mesh connectivity

--- tests/test_wall.py
+++ tests/test_wall.py
@@ -103,6 +103,36 @@ mesh_points = builds(np.meshgrid, grid_1d, grid_1d).map(
 )
 
 
+@given(mesh_points)
+def test_mesh_connections_corners(points: SliceCoords) -> None:
+    connections = get_rectangular_mesh_connections(points)
+    assert len(connections[points[0, 0]]) == 3
+    assert len(connections[points[0, -1]]) == 3
+    assert len(connections[points[-1, 0]]) == 3
+    assert len(connections[points[-1, -1]]) == 3
+
+
+@given(mesh_points)
+def test_mesh_connections_edges(points: SliceCoords) -> None:
+    connections = get_rectangular_mesh_connections(points)
+    shape = points.x1.shape
+    assert all(len(connections[points[0, j]]) == 5 for j in range(1, shape[1] - 1))
+    assert all(len(connections[points[-1, j]]) == 5 for j in range(1, shape[1] - 1))
+    assert all(len(connections[points[i, 0]]) == 5 for i in range(1, shape[0] - 1))
+    assert all(len(connections[points[i, -1]]) == 5 for i in range(1, shape[0] - 1))
+
+
+@given(mesh_points)
+def test_mesh_connections_interior(points: SliceCoords) -> None:
+    connections = get_rectangular_mesh_connections(points)
+    shape = points.x1.shape
+    assert all(
+        len(connections[points[i, j]]) == 8
+        for i in range(1, shape[0] - 1)
+        for j in range(1, shape[1] - 1)
+    )
+
+
 def get_bounds(points: SliceCoords) -> frozenset[SliceCoord]:
     shape = points.x1.shape
     return (
